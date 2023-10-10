f7c11904bc859a6b689c2e0a0be01fa5b0ca08a2
Ahmad Alhasany <ahmadhs271@gmail.com>
2023-10-11 01:33:53 +0300

    wh

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" style = {{backgroundColor: "red"}}>
-      Ahmad Alhasany
+      Ahmad Alhasany Alhasany
     </div>
   );
 }
